69a888fb30c22900e44ff5324e4441237975a327
thegass12 <pee_bv12@hotmail.com>
2015-08-20 13:38:47 +0700

    up lifetime bullet.

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -5,7 +5,7 @@ public class Bullet : MonoBehaviour {
 
 	public int speed = 10;
 	public int power = 1;
-	public float lifeTime = 2;
+	public float lifeTime = 3; //up lifetime
 	void Start () {
 		GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
 		Destroy (gameObject, lifeTime);

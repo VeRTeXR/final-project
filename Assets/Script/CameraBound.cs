f17a1aae1261cd12fc9bb8f78e18e625fd5aa145
VeRTeXR <p5vasut27665@gmail.com>
2016-01-16 03:41:45 +0700

    Camera Bound works

--- Assets/Script/CameraBound.cs
+++ Assets/Script/CameraBound.cs
@@ -3,28 +3,36 @@ using System.Collections;
 
 public class CameraBound : MonoBehaviour {
 
-	private float rightBound;
-	private float leftBound;
-	private float topBound;
-	private float bottomBound;
+	private float minX;
+	private float maxX;
+	private float minY;
+	private float maxY;
+	private float mapX = 100.00f;
+	private float mapY = 20.00f;
+
 	
-	private Vector3 pos;
+	private Vector3 v3;
 	private Transform target;
-	private SpriteRenderer spriteBounds;
+
 	// Use this for initialization
 	void Start ()
 	{
 		float vertExtent = Camera.main.GetComponent<Camera>().orthographicSize;
 		float horzExtent = vertExtent * Screen.width / Screen.height;
 		
-		spriteBounds = GameObject.Find("Background").GetComponentInChildren<SpriteRenderer>();
-		
+
 		target = GameObject.FindWithTag("Player").transform;
 		
-		leftBound = (float)(horzExtent - spriteBounds.sprite.bounds.size.x / 2.0f);
+		minX = (float)(0+2.5);
+		maxX = (float)(100-4);
+		minY = (float)(0+2);
+		maxY = (float)(20-4);
+		//fuck it 
+
+		/*leftBound = (float)(horzExtent - spriteBounds.sprite.bounds.size.x / 2.0f);
 		rightBound = (float)(spriteBounds.sprite.bounds.size.x / 2.0f - horzExtent);
 		bottomBound = (float)(vertExtent - spriteBounds.sprite.bounds.size.y / 2.0f);
-		topBound = (float)(spriteBounds.sprite.bounds.size.y / 2.0f - vertExtent);
+		topBound = (float)(spriteBounds.sprite.bounds.size.y  / 2.0f - vertExtent);*/
 		
 	}
 	// Update is called once per frame
@@ -32,10 +40,10 @@ public class CameraBound : MonoBehaviour {
 	{
 		//Debug.Log();
 		
-		var pos = new Vector3(target.position.x, target.position.y, transform.position.z);
-		pos.x = Mathf.Clamp(pos.x, leftBound, rightBound);
-		pos.y = Mathf.Clamp(pos.y, bottomBound, topBound);
-		transform.position = pos;
+		var v3 = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		v3.x = Mathf.Clamp(v3.x, minX, maxX);
+		v3.y = Mathf.Clamp(v3.y, minY, maxY);
+		transform.position = v3;
 	}
 	
 	void OnLevelWasLoaded()

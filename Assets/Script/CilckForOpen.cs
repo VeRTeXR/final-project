370662bf8cbc9289026b32b644df09a83f716abb
thegass12 <pee_bv12@hotmail.com>
2016-02-08 12:11:52 +0700

    add start scn.

---
+++ Assets/Script/CilckForOpen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CilckForOpen : MonoBehaviour {
+
+	/*void OnMouseDown(){
+
+		Application.LoadLevel ("mission");
+
+	}*/
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            Application.LoadLevel("mission");
+
+        }
+
+
+
+    }
+
+}
---
+++ Assets/Script/SpBarController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpBarController : MonoBehaviour {
+
+
+    public GameObject healthBar;
+    public float curSp;
+    public float maxSP = 10;
+    public float miinSp;
+    public float chargeTime;
+
+    // Use this for initialization
+    void Start () {
+
+        curSp = maxSP;
+	
+	}
+	
+	// Update is called once per frame
+	
+
+    public void decreseBar() {
+
+        curSp -= 5;
+
+        float calSpBar = curSp / maxSP;
+
+        seSpbar(calSpBar);
+
+
+    }
+
+    public void increseBar() {
+
+        curSp += 2;
+
+        float calSpBar = curSp / maxSP;
+
+        seSpbar(calSpBar);
+
+    }
+
+    public void seSpbar(float a)
+    {
+
+        healthBar.transform.localScale = new Vector3(a, transform.localScale.y, transform.localScale.z);
+
+
+
+    }
+
+    void Update()
+    {
+
+        if (curSp < maxSP)
+        {
+
+            chargeTime += Time.deltaTime;
+
+        }
+
+        if (chargeTime == 3)
+        {
+
+            increseBar();
+
+            chargeTime = 0;
+
+        }
+
+
+
+
+
+
+    }
+
+}

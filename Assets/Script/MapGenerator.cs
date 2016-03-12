74dc21515c281e65467d89919cf6ba0bb8d522fd
VeRTeXR <p5vasut27665@gmail.com>
2016-03-13 03:55:36 +0700

    works but need serious sprite optimisation cuz memory buffer overflow

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -1,61 +1,113 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour {
-
+	
 	public int width;
 	public int height;
+	
 	public string seed;
 	public bool useRandomSeed;
+	public GameObject[] tileArray;
+	public GameObject tileChoice;
 
 	[Range(0,100)]
-	public int  randomFillPercent;
-
-	int [,] map;
-
-	// Use this for initialization
-	void Start () {
-		GenerateMap ();
+	public int randomFillPercent;
 	
+	int[,] map;
+	
+	void Start() {
+		GenerateMap();
 	}
-
-	void GenerateMap (){
-
-		map = new int [width, height];
-		RandomFillMap ();
+	
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			GenerateMap();
+		}
 	}
-
+	
+	void GenerateMap() {
+		map = new int[width,height];
+		RandomFillMap();
+		
+		for (int i = 0; i < 5; i ++) {
+			SmoothMap();
+		}
+	}
+	
+	
 	void RandomFillMap() {
 		if (useRandomSeed) {
-			seed = Time.time.ToString ();
+			seed = Time.time.ToString();
 		}
-		System.Random psudoRand = new System.Random (seed.GetHashCode ());
-	
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				if(x==0||x==width-1||y==0||y==height-1){
-					map[x,y]= 1;
+		
+		System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+		
+		for (int x = 0; x < width; x ++) {
+			for (int y = 0; y < height; y ++) {
+				if (x == 0 || x == width-1 || y == 0 || y == height -1) {
+					map[x,y] = 1;
 				}
 				else {
-				map [x, y] = (psudoRand.Next (0, 100) < randomFillPercent) ? 1 : 0;
+					map[x,y] = (pseudoRandom.Next(0,100) < randomFillPercent)? 1: 0;
 				}
 			}
 		}
 	}
-
+	
 	void SmoothMap() {
-	}
-
-		void onDraw () {
-			if(map != null){
+		for (int x = 0; x < width; x ++) {
+			for (int y = 0; y < height; y ++) {
+				int neighbourWallTiles = GetSurroundingWallCount(x,y);
 				
-				for (int x = 0; x < width; x++) {
-					for(int y = 0; y < height; y++){
-						Vector3 pos = new Vector3 (-width/2+x+.5f,0,-height/2+y+.5f);
+				if (neighbourWallTiles > 4)
+					map[x,y] = 1;
+				else if (neighbourWallTiles < 4)
+					map[x,y] = 0;
+				
+			}
+		}
+	}
+	
+	int GetSurroundingWallCount(int gridX, int gridY) {
+		int wallCount = 0;
+		for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX ++) {
+			for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY ++) {
+				if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height) {
+					if (neighbourX != gridX || neighbourY != gridY) {
+						wallCount += map[neighbourX,neighbourY];
 					}
 				}
+				else {
+					wallCount ++;
+				}
 			}
 		}
+		
+		return wallCount;
+	}
+	
+	
+	void OnDrawGizmos() {
+		if (map != null) {
+			for (int x = 0; x < width; x ++) {
+				for (int y = 0; y < height; y ++) {
+					//Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+					Vector3 pos = new Vector3(-width/2 + x + .5f,-height/2 + y+.5f, 1);
+					if(map[x,y]==1) {
+						tileChoice = tileArray [0];
+					}
+					else {
+						tileChoice = tileArray [1];
+					}
+					Instantiate (tileChoice, pos, Quaternion.identity);
 
+					//Gizmos.DrawCube(pos,new Vector3(1, 1, .5f));
+				}
+			}
+		}
+	}
+	
 }

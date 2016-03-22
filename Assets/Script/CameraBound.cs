77f8845759439691ddf78db4dc7b41d45ad2037c
VeRTeXR <p5vasut27665@gmail.com>
2016-03-22 17:00:34 +0700

    camera bound and player bound now scale accordingly to the scene size // still need to build the wall

--- Assets/Script/CameraBound.cs
+++ Assets/Script/CameraBound.cs
@@ -3,31 +3,30 @@ using System.Collections;
 
 public class CameraBound : MonoBehaviour {
 
+	private GameObject map;
 	private float minX;
 	private float maxX;
 	private float minY;
 	private float maxY;
-	private float mapX = 100.00f;
-	private float mapY = 20.00f;
 
-	
+
+	private float width;
+	private float height;
 	private Vector3 v3;
 	private Transform target;
 
-	// Use this for initialization
 	void Start ()
 	{
+		map = GameObject.Find ("CellularAutomata");
+		width = map.GetComponent<MapGenerator>().width;
+		height = map.GetComponent<MapGenerator>().height;
 		float vertExtent = Camera.main.GetComponent<Camera>().orthographicSize;
 		float horzExtent = vertExtent * Screen.width / Screen.height;
 		
-
-		target = GameObject.FindWithTag("Player").transform;
-		
-		minX = (float)(0+3.5);
-		maxX = (float)(50-4.7);
-		minY = (float)(0+2);
-		maxY = (float)(20-4);
-		//fuck it 
+		minX = (float)(horzExtent - width/2.0f); 
+		maxX = (float)(width/2.0f-horzExtent);
+		minY = (float)(vertExtent-height/2.0f);
+		maxY = (float)(height/2.0f-vertExtent);
 
 		/*leftBound = (float)(horzExtent - spriteBounds.sprite.bounds.size.x / 2.0f);
 		rightBound = (float)(spriteBounds.sprite.bounds.size.x / 2.0f - horzExtent);
@@ -35,11 +34,9 @@ public class CameraBound : MonoBehaviour {
 		topBound = (float)(spriteBounds.sprite.bounds.size.y  / 2.0f - vertExtent);*/
 		
 	}
-	// Update is called once per frame
+
 	void Update ()
 	{
-		//Debug.Log();
-		
 		var v3 = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 		v3.x = Mathf.Clamp(v3.x, minX, maxX);
 		v3.y = Mathf.Clamp(v3.y, minY, maxY);

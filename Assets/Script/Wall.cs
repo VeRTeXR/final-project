8a30d9e653cbe68452e4bab54e399885f3410c81
VeRTeXR <p5vasut27665@gmail.com>
2016-03-23 13:41:11 +0700

    add wall script

--- Assets/Script/Wall.cs
+++ Assets/Script/Wall.cs
@@ -3,13 +3,44 @@ using System.Collections;
 
 public class Wall : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public int wallHp;
+	public int randIndex;
+	public GameObject wallPieces;
+
+
+	void OnCollisionEnter2D(Collision2D c){
+
+		if (c.gameObject.tag == "PlayerBullet") {
+			wallHp = wallHp - 1;
+			if (wallHp <= 0) {
+				onContact ();
+				Destroy (c.gameObject);
+			}
+		}
+		if (c.gameObject.tag == "EnemyBullet") {
+			wallHp = wallHp - 1;
+			if (wallHp <= 0) {
+				onContact ();
+				Destroy (c.gameObject);
+			}
+		}
+		if (c.gameObject.tag == "Player") {
+			onContact ();
+		}
+		if (c.gameObject.tag == "Enemy") {
+			onContact ();
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void onContact () {
+		randIndex = Random.Range (1,3);
+		for (int i = 1; i < randIndex; i++) {
+			GameObject b = Instantiate(wallPieces, transform.position, Quaternion.identity) as GameObject;
+			b.GetComponent<Rigidbody2D>().AddForce(Vector3.right * Random.Range(-400, 400));
+			b.GetComponent<Rigidbody2D>().AddForce(Vector3.up * Random.Range(-200, 200));
+		}
+		Destroy (gameObject);
 	}
+
+
 }
